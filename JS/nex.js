38a6d54eed2bc4c43ceaf9c2919d245c6f6191f5
Cacau Lima <cacaualcantaralima@gmail.com>
2021-08-25 08:31:28 -0300

    primeiro exercicio de js

---
+++ JS/nex.js
@@ -0,0 +1,16 @@
+var nome = prompt ("Qual é o seu nome?");
+var idade = prompt ("Qual a sua idade?");
+var serie = prompt ("Qual a sua série?");
+
+var nome = document.querySelector("h1");
+var idade = document.querySelector("#idade");
+var serie = document.querySelector("#serie");
+
+document.write(nome);
+
+document.write(idade);
+
+document.write(serie);
+
+
+
