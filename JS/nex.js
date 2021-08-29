bbaffa93cad6d779bc6831e7e3e7c42a8bf09baa
Cacau Lima <cacaualcantaralima@gmail.com>
2021-08-29 12:54:43 -0300

    outro exercicio de js

--- JS/nex.js
+++ JS/nex.js
@@ -1,16 +1,47 @@
+document.getElementById("nome").innerHTML= "Meu nome é "+prompt('Qual o seu nome?');
+
+document.getElementById("idade").innerHTML= "Minha idade é "+prompt('Qual a sua idade?');
+
+document.getElementById("serie").innerHTML= "Minha série é "+prompt('Qual a sua serie?');
+
+/*
+
+3
+
 var nome = prompt ("Qual é o seu nome?");
 var idade = prompt ("Qual a sua idade?");
 var serie = prompt ("Qual a sua série?");
+
+document.getElementById("nome").innerHTML= ;
+
+1
 
 var nome = document.querySelector("h1");
 var idade = document.querySelector("#idade");
 var serie = document.querySelector("#serie");
 
-document.write(nome);
+document.write(h1);
 
-document.write(idade);
+document.write(p);
 
-document.write(serie);
+document.write(p);
+
+2
+
+function nome() {
+    let answer = prompt("Qual é o seu nome");
+}
+
+function idade() {
+    let answer = prompt("Qual é a sua idade?");
+}
+
+function serie() {
+    let answer = prompt("Qual é a sua série?");
+
+}*/
+
+
 
 
 
