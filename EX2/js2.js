aebed004f70559087c14d0e109363837124cbbb8
Cacau Lima <cacaualcantaralima@gmail.com>
2021-08-29 12:37:01 -0300

    outro exercicio js2

--- EX2/js2.js
+++ EX2/js2.js
@@ -1,4 +1,11 @@
-function js_style () {
-    document.getElementById('jsstyle').style.backgroundColor= #87CEFA;
+function blue(){
+    document.getElementById("change").style.color = "blue";
 }
 
+function red(){
+    document.getElementById("change").style.color = "red";
+}
+
+function yellow(){
+    document.getElementById("change").style.color = "yellow";
+}
